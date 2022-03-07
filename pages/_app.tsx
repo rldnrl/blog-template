3b8e6a23d56f90810aeb74f5c7dff3811edee827
rldnrl <devpark1002@gmail.com>
2022-03-07 22:01:00 +0900

    emotion normalize 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Global, css } from '@emotion/react'
+import emotionNormalize from 'emotion-normalize'
+
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          ${emotionNormalize}
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: Inter, ui-sans-serif, system-ui, -apple-system,
+              BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial,
+              Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji,
+              Segoe UI Symbol, Noto Color Emoji;
+          }
+          * {
+            box-sizing: border-box;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
