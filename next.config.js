0a49c531660035c99b93e93f0feb70d3359224d7
rldnrl <devpark1002@gmail.com>
2022-03-07 22:07:04 +0900

    sass 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
