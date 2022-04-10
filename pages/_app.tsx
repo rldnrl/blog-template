8c4ff235fd798ec3bd4571f02ffee2bc67435479
rldnrl <devpark1002@gmail.com>
2022-04-10 22:51:32 +0900

    feat: next ui를 사용하기 위한 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,13 @@
 import type { AppProps } from 'next/app'
-import { Global, css } from '@emotion/react'
-import emotionNormalize from 'emotion-normalize'
+import { NextUIProvider } from '@nextui-org/react'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Global
-        styles={css`
-          ${emotionNormalize}
-          html,
-          body {
-            padding: 0;
-            margin: 0;
-            font-family: Inter, ui-sans-serif, system-ui, -apple-system,
-              BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial,
-              Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji,
-              Segoe UI Symbol, Noto Color Emoji;
-          }
-          * {
-            box-sizing: border-box;
-          }
-        `}
-      />
+    <NextUIProvider>
       <Component {...pageProps} />
-    </>
+    </NextUIProvider>
   )
 }
 
