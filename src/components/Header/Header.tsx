9d631e27f5caa43f824a46f28edf3e63c5ea2b39
rldnrl <devpark1002@gmail.com>
2022-04-10 22:44:21 +0900

    feat: 파일 옮김

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 import Link from 'next/link'
 import { css } from '@emotion/react'
 
-import List from '@/components/List'
+import List from 'src/components/List'
 
 type HeaderProps = {
   children: ReactNode
